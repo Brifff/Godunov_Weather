d20b2a6756be2f5033f17d9e2da5a99ecca9c645
Ataev.m.N <104029906+Brifff@users.noreply.github.com>
2023-03-20 11:39:44 +0300

    weather init

---
+++ Godunov_Weather/Godunov/citiesNameCell.swift
@@ -0,0 +1,26 @@
+//
+//  citiesNameCell.swift
+//  Godunov
+//
+//  Created by Годунов Антон Иванович on 13/2/2023.
+//
+
+import UIKit
+
+class citiesNameCell: UITableViewCell {
+    
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var cityTemp: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
